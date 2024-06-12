1b1bed9f819a250380b728a473a162620b878034
mehmetCt <mehmet@cybertekschool.com>
2024-06-12 15:59:29 -0400

    Evening OH - June 12th

---
+++ src/test/java/com/cydeo/liveLabs/week26/evening/Homework1.java
@@ -0,0 +1,33 @@
+package com.cydeo.liveLabs.week26.evening;
+
+public class Homework1  {
+
+    /*
+
+     * Task 1 :
+     * - Given accept type is Json
+     * - When users sends request to /countries/US
+     * - Then status code is 200
+     * - And Content - Type is application/json
+     * - And response contains United States of America
+
+
+     * Task 2 : NEGATIVE TESTS
+     * - Given accept type is Json
+     * - When users sends request to /employees/1
+     * - Then status code is 404
+
+
+     * Task 3 :
+     * - Given Accept type  is Json
+     * - When users sends request to /regions/1
+     * - Then status code is 200
+     * - And Content - Type is application/json
+     * - And response contains Europe
+     * - And header should contains Date
+     * - And "Transfer-Encoding" should be "chunked"
+
+
+     */
+
+}
