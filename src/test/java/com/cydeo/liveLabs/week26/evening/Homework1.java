9b9f9d3622a65a36499083961f0c5f56e29bb559
mehmetCt <mehmet@cybertekschool.com>
2024-06-12 16:49:29 -0400

    Evening OH - June 12th

--- src/test/java/com/cydeo/liveLabs/week26/evening/Homework1.java
+++ src/test/java/com/cydeo/liveLabs/week26/evening/Homework1.java
@@ -1,6 +1,14 @@
 package com.cydeo.liveLabs.week26.evening;
 
-public class Homework1  {
+import com.cydeo.utility.HrTestBase;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Homework1  extends HrTestBase {
 
     /*
 
@@ -29,5 +37,39 @@ public class Homework1  {
 
 
      */
+
+    @Test
+    public void task1() {
+
+        Response response = RestAssured.given().log().uri()
+                .accept(ContentType.JSON)
+                .when().get("/countries/US");
+
+        response.prettyPrint();
+
+        //     * - Then status code is 200
+        Assertions.assertEquals(200,response.statusCode());
+
+
+        //     * - And Content - Type is application/json
+        Assertions.assertEquals(ContentType.JSON.toString(),response.contentType());
+
+
+        //     * - And response contains United States of America
+        Assertions.assertTrue(response.body().asString().contains("United States of America"));
+
+
+        // Response Path
+        String countryName = response.path("country_name");
+        Assertions.assertEquals("United States of America",countryName);
+
+
+        // JsonPath
+        JsonPath jp = response.jsonPath();
+        String cName = jp.getString("country_name");
+        Assertions.assertEquals("United States of America",cName);
+
+
+    }
 
 }
