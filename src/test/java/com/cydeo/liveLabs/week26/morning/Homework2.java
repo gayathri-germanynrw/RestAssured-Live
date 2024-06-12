e96fdefa2d2a24b0d534c97af1f916ecf3c848b3
arthur-rule <arthur.rule@cydeo.com>
2024-06-12 20:46:49 +0300

    06/11/2024

--- src/test/java/com/cydeo/liveLabs/week26/morning/Homework2.java
+++ src/test/java/com/cydeo/liveLabs/week26/morning/Homework2.java
@@ -1,38 +1,44 @@
 package com.cydeo.liveLabs.week26.morning;
 
-public class Homework2  {
-    /*
-     * TASK 1 :
-     * - Given accept type is Json
-     * - Path param value- US
-     * - When users sends request to /countries
-     * - Then status code is 200
-     * - And Content - Type is Json
-     * - And country_id is US
-     * - And Country_name is United States of America
-     * - And Region_id is 2
+import com.cydeo.utility.HrTestBase;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-     * TASK 2 :
-     * - Given accept type is Json
-     * - Query param value - q={"department_id":80}
-     * - When users sends request to /employees
-     * - Then status code is 200
-     * - And Content - Type is Json
-     * - And all job_ids start with 'SA'
-     * - And all department_ids are 80
-     * - Count is 25
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-     * TASK 3 :
-     * - Given accept type is Json
-     * - Query param value q={“region_id":3}
-     * - When users sends request to /countries
-     * - Then status code is 200
-     * - And all regions_id is 3
-     * - And count is 6
-     * - And hasMore is false
-     * - And Country_name are;
-     * Australia,China,India,Japan,Malaysia,Singapore
-     */
+public class Homework2 extends HrTestBase {
+    @Test
+    public void task1() {
+// * - Given accept type is Json
+// * - Path param value- US
+// * - When users sends request to /countries
 
+        Response response = given().accept(ContentType.JSON)
+                                    .pathParam("country_id", "US")
+                                    .when().get("/countries/{country_id}");
+        //response.prettyPrint();
 
+//* - Then status code is 200
+
+        assertEquals(HttpStatus.SC_OK,response.statusCode());
+
+//* - And Content - Type is Json
+
+        assertEquals(ContentType.JSON.toString(), response.contentType());
+
+//* - And country_id is US
+        JsonPath jsonPath= response.jsonPath();
+        assertEquals("US",jsonPath.getString("country_id"));
+
+//* - And Country_name is United States of America
+        assertEquals("United States of America",jsonPath.getString("country_name"));
+
+//* - And Region_id is 2
+        assertEquals(2,jsonPath.getInt("region_id"));
+    }
 }
