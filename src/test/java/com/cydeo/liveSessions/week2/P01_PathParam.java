fb0f5d659799c021e5d175996933134b218677ed
mehmetCt <mehmet@cybertekschool.com>
2024-06-15 12:18:36 -0400

    Live Session - Week2

--- src/test/java/com/cydeo/liveSessions/week2/P01_PathParam.java
+++ src/test/java/com/cydeo/liveSessions/week2/P01_PathParam.java
@@ -1,6 +1,9 @@
 package com.cydeo.liveSessions.week2;
 
 import com.cydeo.utility.FakeStoreTestBase;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class P01_PathParam extends FakeStoreTestBase {
@@ -15,11 +18,18 @@ public class P01_PathParam extends FakeStoreTestBase {
      * - Content Type is application/json; charset=utf-8
      * - Print response
      * - id is 60
-     * - Name is "My Products"
-     * - Category name is "Shoes"
+     * - Name is "My Product"
+     * - Category name is "Organic"
      */
     @Test
     public void responsePath() {
+
+        Response response = RestAssured.given().accept(ContentType.JSON)
+                .pathParam("id", 60)
+                .when().get("/products/{id}");
+
+        // Print response
+        response.prettyPrint();
 
     }
 }
